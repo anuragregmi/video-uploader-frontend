e53ed3a7ee24360a0cfc16ec2f373039803640ce
Anurag Regmi <anuragregmi@protonmail.com>
2021-08-07 14:16:48 +0545

    Add quality selector to video player

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const token = window.localStorage.getItem('authToken')
 const $axios = axios.create({
     baseURL: 'http://localhost:8000/',
-    timeout: 1000,
     headers: token ? { 'Authorization': `token ${token}` } : {}
 });
